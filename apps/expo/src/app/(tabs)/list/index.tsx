7dee37296ab97379c1154ec9a6b223672853aad9
Raman Halder <ramanhalder12@gmail.com>
2023-06-15 13:13:46 +0530

    expo: list: minor-changes View to Touchable

--- apps/expo/src/app/(tabs)/list/index.tsx
+++ apps/expo/src/app/(tabs)/list/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
 
@@ -7,46 +7,47 @@ const ActionBar: React.FC = () => {
     return (
         <View className='my-5 flex flex-row items-center w-full justify-between'>
             <View className='flex flex-col items-center justify-center'>
-                <View className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
+                <TouchableOpacity className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
                     <Feather name="user-plus" color={'#ff9580'} size={25} />
-                </View>
+                </TouchableOpacity>
                 <Text className='text-sm font-semibold text-center leading-6 text-leadistroWhite'>Add Contacts</Text>
             </View>
             <View className='flex flex-col items-center justify-center'>
-                <View className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
+                <TouchableOpacity className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
                     <Feather name="box" color={'#ff9580'} size={25} />
-                </View>
+                </TouchableOpacity>
                 <Text className='text-sm font-semibold text-center leading-6 text-leadistroWhite'>Inbox</Text>
             </View>
             <View className='flex flex-col items-center justify-center'>
-                <View className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
+                <TouchableOpacity className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
                     <Feather name="bell" color={'#ff9580'} size={25} />
-                </View>
+                </TouchableOpacity>
                 <Text className='text-sm font-semibold text-center leading-6 text-leadistroWhite'>Subscribed</Text>
             </View>
             <View className='flex flex-col items-center justify-center'>
-                <View className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
+                <TouchableOpacity className='px-4 py-4 mb-2 rounded-xl border-2 border-leadistroRed'>
                     <Feather name="user-check" color={'#ff9580'} size={25} />
-                </View>
+                </TouchableOpacity>
                 <Text className='text-sm font-semibold text-center leading-6 text-leadistroWhite'>VIPs</Text>
             </View>
         </View>
     );
 }
+
 const TasksBar: React.FC = () => {
     return (
         <>
             <View className='bg-leadistroBrown rounded-xl space-y-2 flex flex-col items-start p-4 justify-start w-full h-[80vh]'>
-                <Text className=' font-bold tracking-wider text-leadistroRed'>
+                <Text className='font-bold tracking-wider text-leadistroRed'>
                     Leads Growth
                 </Text>
-                <Text className=' font-bold tracking-wider text-leadistroRed'>
+                <Text className='font-bold tracking-wider text-leadistroRed'>
                     Engagement
                 </Text>
-                <Text className=' font-bold tracking-wider text-leadistroRed'>
+                <Text className='font-bold tracking-wider text-leadistroRed'>
                     Subscribers Today
                 </Text>
-                <Text className=' font-bold tracking-wider text-leadistroRed'>
+                <Text className='font-bold tracking-wider text-leadistroRed'>
                     Imported Leads History
                 </Text>
             </View>
