5785bc3054c844ae513aa396b1a1caae2746f190
Raman Halder <ramanhalder12@gmail.com>
2023-06-24 23:43:38 +0530

    nextjs: homeScreen

--- apps/nextjs/src/components/HomeScreen.tsx
+++ apps/nextjs/src/components/HomeScreen.tsx
@@ -9,6 +9,9 @@ import IMG from '../../public/OutlineMockup.png';
 import BackgroundAnimation from './BackgroundAnimations'
 import { SizeContext } from '~/utils/size-observer'
 import { AnimatePresence, motion as m } from 'framer-motion'
+import { Separator } from './ui/seperator'
+import { ArrowRightCircleIcon } from 'lucide-react'
+import { Input } from './ui/input'
 // import { RGradientCard } from './RadialGradientCards'
 
 export default function HomeScreen() {
@@ -17,7 +20,7 @@ export default function HomeScreen() {
         setImageLoaded(true);
     }, [])
     const { innerWidth } = React.useContext(SizeContext);
-    const ratio = innerWidth < 640 ? (1 / 2) : (3 / 4);
+    const ratio = innerWidth < 640 ? (1 / 2) : (1 / 1);
     return (
         <>
             <div
@@ -63,7 +66,7 @@ export default function HomeScreen() {
                     </AnimatePresence>
                 </div>
                 <div className="container flex h-auto flex-col items-center justify-center md:py-36">
-                    <AspectRatio ratio={ratio} className={` transition-all duration-1000 ${imageLoaded ? 'opacity-100 scale-95' : 'opacity-0 '}`}>
+                    <AspectRatio ratio={ratio} className={` transition-all duration-1000 ${imageLoaded ? 'opacity-100 scale-[0.96]' : 'opacity-0 '}`}>
                         <Image src={IMG} alt="MobileMockup" onLoad={handleImageLoaded} className='pointer-events-none object-center md:object-contain object-cover w-full h-full ' />
                     </AspectRatio>
                 </div>
@@ -77,20 +80,29 @@ export default function HomeScreen() {
                     <Heading4 textChildren='Sign In To Access All Beta Features' className='font-poppins text-leadistroBlack text-center md:text-left mix-blend-color-burn' />
                 </div>
                 {/* Initiate bento grids design here use tailwind css container queries  */}
-                <div className="grid min-h-screen max-w-screen-lg md:max-w-screen-xl w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 py-5 px-4 md:py-10 md:px-16">
-                    <div className="bg-leadistroBlack/80 border-2 flex flex-col items-center justify-center border-leadistroWhite/60 backdrop-blur-sm rounded-3xl @container ">
-                        <Heading2 textChildren="Minimal Marketing Tool" className="font-poppins text-leadistroWhite" />
+                <div className="grid min-h-screen max-w-screen-lg md:max-w-screen-xl font-poppins w-full grid-cols-2 lg:grid-cols-3 gap-2 md:gap-3 py-5 px-4 md:py-10 md:px-16">
+                    <div className="bg-leadistroBlack/80 border-2 border-leadistroWhite/70 flex flex-col items-start justify-center w-auto h-auto backdrop-blur-sm md:col-span-2 rounded-3xl @container ">
+                        <h1 className='mx-5 font-poppins max-w-lg mb-4 text-leadistroWhite text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold md:font-extrabold text-center md:text-left'>
+                            Create Your Own
+                            Custom Campaign
+                        </h1>
+                        <Separator className='w-full bg-leadistroWhite' />
+                        <div className='mx-5 mt-8 flex items-start justify-between w-full h-auto  font-poppins text-leadistroWhite text-center md:text-left text-xs md:text-sm lg:text-base xl:text-lg'>
+                            <p>Integrate Generative Ai With The Help Of Artificial Intelligence.</p>
+                            <ArrowRightCircleIcon className='text-leadistroRed md:w-7 md:h-7 w-5 h-5 mr-10 cursor-pointer' />
+                        </div>
+                    </div>
+                    <div className="flex relative flex-col items-center justify-between h-full w-auto bg-gradient-to-b from-leadistroBlack/80 via-leadistroBrown/60 to-leadistroBlack/60 md:row-span-5 rounded-3xl @container">
+                        <div className='w-full h-[25%] bg-leadistroBlack rounded-t-3xl'>
+                            <h3 className='text-leadistroWhite p-6 text-center md:text-left text-xs md:text-sm lg:text-base xl:text-lg font-poppins font-bold'>How Does It Work You Wanna know?</h3>
+                        </div>
+                        <div className='w-[80%] absolute left-4 bottom-44 right-0 h-[40%] bg-leadistroBrown rounded-2xl'></div>
+                        <Input placeholder='How do you work?' className='pointer-events-none w-full h-10 bg-leadistroBrown border-t-[1px] border-t-leadistroRed border-b-leadistroBlack border-x-leadistroBlack text-leadistroWhite rounded-3xl'></Input>
+                    </div>
+                    <div className="bg-leadistroBlack/80 border-2 row-span-4 flex flex-col items-center justify-center border-leadistroWhite/60 backdrop-blur-sm rounded-3xl @container ">
 
                     </div>
-                    <div className="bg-gradient-to-b border-2 border-leadistroWhite/50 from-leadistroBlack/70 to-leadistroBrown/70 backdrop-blur-sm  rounded-3xl @container ">
-
-                    </div>
-                    <div className="bg-leadistroBrown md:row-span-2 rounded-3xl @container ">
-
-                    </div>
-
-                    <div className="bg-leadistroBrown/40 backdrop-blur-sm md:col-span-2 rounded-3xl @container ">
-
+                    <div className="bg-gradient-to-b border-2 row-span-4  border-leadistroWhite/50 from-leadistroBlack/70 to-leadistroBrown/70 backdrop-blur-sm  rounded-3xl @container ">
                     </div>
                 </div>
             </div >
