0367ddb8da96fd860ec9c37901073bb99017302e
Raman Halder <ramanhalder12@gmail.com>
2023-07-09 16:14:45 +0530

    Re checking Endpoint

---
+++ packages/api/src/router/contacts.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const LeadsContactRouter = createTRPCRouter({
+  all: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.lead.findMany({ orderBy: { id: "desc" } });
+  }),
+  byId: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.lead.findFirst({ where: { id: input.id } });
+    }),
+  create: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        emailAddress: z.string().email(),
+        vip: z.boolean(),
+        subscribed: z.boolean(),
+        Campaign: z.any().optional(),
+        campaignId: z.string().optional(),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.lead.create({ data: input });
+    }),
+  delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
+    return ctx.prisma.lead.delete({ where: { id: input } });
+  }),
+});
